9e4661908df84a71e59c1895da3d728c7c7c7b06
Dallin <dallinpoulson@gmail.com>
2020-06-15 20:32:08 -0600

    working on option toggler with state and switch statement

--- src/Components/MaintenanceRequest/MaintenanceRequest.js
+++ src/Components/MaintenanceRequest/MaintenanceRequest.js
@@ -1,12 +1,62 @@
-import React,{ useState } from 'react'
-
+import React, { useState } from "react";
+import {
+  Form,
+  Button,
+  ToggleButtonGroup,
+  ToggleButton,
+  Row,
+  Col,
+} from "react-bootstrap";
 
 const MaintenanceRequest = () => {
-    return (
-        <div>
-            submit requests here hello
-        </div>
-    )
-}
+  const options = [
+    "emergency",
+    "bathroom",
+    "bedroom",
+    "yard",
+    "kitchen",
+    "living_room",
+    "exterior",
+    "appliance",
+    "plumbing",
+    "electrical",
+    "other",
+  ];
+  const [emergency, toggleEmergency] = useState(false);
+  const optionToggler = (option) => {
+    switch (option) {
+      case "emergency":
+        toggleEmergency(!emergency)
+        console.log(emergency);
+        break;
+        default:
+            console.log('default')
+    }
+  };
+  return (
+    <div>
+      <Form>
+        {options.map((option) => {
+          let capitalized = option.charAt(0).toUpperCase() + option.slice(1);
+          return <button onClick={() => optionToggler(option)}>{capitalized}</button>;
+        })}
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Please explain your request in detail:</Form.Label>
+          <Form.Control type="email" placeholder="Explain here" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.File />
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="Light" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
-export default MaintenanceRequest
+export default MaintenanceRequest;
