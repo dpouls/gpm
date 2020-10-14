cfdc6b78af2ba76c54f13fc135225127b0c9d038
Dallin <dallinpoulson@gmail.com>
2020-10-13 21:28:35 -0600

    fixed bugs with new renter form. Moved the form code to a new component to clean up admin portal code. Up next: axios request to actually create the renter

--- src/Components/AdminPortal/AdminPortal.js
+++ src/Components/AdminPortal/AdminPortal.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import "./AdminPortal.scss";
 import NewRenter from "./NewRenter/NewRenter";
-import swal from "sweetalert";
 
 const AdminPortal = (props) => {
   //Makes sure the user has admin authorization on mounting
@@ -15,54 +14,14 @@ const AdminPortal = (props) => {
       }
     });
   }, []);
+
   //declare state variables
   const [rentersClicked, toggleRentersClicked] = useState(true);
   const [newRenterClicked, toggleNRClicked] = useState(true);
-  const [adminChecked, toggleAdminChecked] = useState(false);
-  const [newRenterForm, setNewRenterForm] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phoneNumber: "",
-    username: "",
-    password: "",
-    occupants: "",
-    isadmin: false,
-  });
-  useEffect(() => {
-    generateUsername();
-    console.log(generatedUsername);
-    console.log(newRenterForm);
-  }, [newRenterForm.phoneNumber]);
-
   const [propertiesClicked, togglePropertiesClicked] = useState(false);
   const [paymentsClicked, togglePaymentsClicked] = useState(false);
   const [mRClicked, toggleMRClicked] = useState(false);
-  const [generatedUsername, setGeneratedUsername] = useState("");
-  const [usernameWasGenerated, toggleUsernameGenerated] = useState(false)
 
-  //generates a username off of the firstName lastName and last four digits of the phone number. Can be changed if desired.
-  const generateUsername = async () => {
-    // if(!usernameWasGenerated){
-      if (newRenterForm.phoneNumber.length > 9) {
-      let lastFour = /[0-9]{4}$/;
-      let lastFourPhone = lastFour.exec(newRenterForm.phoneNumber);
-      await setGeneratedUsername(
-        newRenterForm.firstName + newRenterForm.lastName + lastFourPhone
-      );
-      await setNewRenterForm({ ...newRenterForm, username: generatedUsername });
-      toggleUsernameGenerated(true);
-
-    // }
-    }
-    
-    console.log("generate username didnt fire yet");
-  };
-  //handles all input fields and setting their values to state.
-  const inputHandler = (e) => {
-    setNewRenterForm({ ...newRenterForm, [e.target.name]: e.target.value });
-  };
-  console.log("lets see username:", newRenterForm.username);
   return (
     <div className="admin-portal">
       <button onClick={() => toggleRentersClicked(!rentersClicked)}>
@@ -79,87 +38,7 @@ const AdminPortal = (props) => {
               Add a New Renter
             </button>
           </div>
-          {newRenterClicked ? (
-            //seperate component but i wanna make sure i update it correctly
-            // <NewRenter setNewRenterFormFn={setNewRenterForm}/>
-            <form className="new-renter-form">
-              <label for="firstName">First Name</label>
-              <input name="firstName" type="text" onChange={inputHandler} />
-              <label for="lastName">Last Name</label>
-              <input name="lastName" type="text" onChange={inputHandler} />
-              <label for="email">Email</label>
-              <input name="email" type="email" onChange={inputHandler} />
-              <label for="phoneNumber">Phone Number</label>
-              <input
-                name="phoneNumber"
-                type="text"
-                maxLength="10"
-                onChange={inputHandler}
-              />
-              <label for="username">Username</label>
-              <input
-                value={newRenterForm.username}
-                name="username"
-                type="text"
-                onFocus={generateUsername}
-                onChange={(e) => {
-                  toggleUsernameGenerated(false)
-                  setGeneratedUsername(e.target.value);
-                  inputHandler(e);
-                }}
-              />
-              <label for="password">Password</label>
-              <input name="password" type="password" onChange={inputHandler} />
-              <label for="occupants">Occupants</label>
-              <input name="occupants" type="number" onChange={inputHandler} />
-
-              <label className="admin-checkbox-container" for="adminCheckbox">
-                Admin:{" "}
-                {adminChecked ? (
-                  <>
-                    <button
-                      name="adminCheckbox"
-                      id="admin-button-checked"
-                      onClick={() => toggleAdminChecked(!adminChecked)}
-                    >
-                      Yes
-                    </button>
-                    <span>
-                      Click if you wish to remove admin controls from this user.
-                    </span>
-                  </>
-                ) : (
-                  <>
-                    <button
-                      name="adminCheckbox"
-                      id="admin-button-not-checked"
-                      onClick={() => {
-                        swal({
-                          title: "Warning!",
-                          text:
-                            "Checking this box will alow this user admin access!",
-                          icon: "warning",
-                          buttons: true,
-                          dangerMode: true,
-                        }).then((okay) => {
-                          if (okay) {
-                            toggleAdminChecked(!adminChecked);
-                          } else {
-                            swal("This user was not marked as an admin.");
-                          }
-                        });
-                      }}
-                    >
-                      No
-                    </button>
-                    <span>
-                      Click if you wish to give this user admin controls.
-                    </span>
-                  </>
-                )}
-              </label>
-            </form>
-          ) : null}
+          {newRenterClicked ? <NewRenter /> : null}
         </div>
       ) : null}
       <button onClick={() => togglePropertiesClicked(!propertiesClicked)}>
--- src/Components/AdminPortal/NewRenter/NewRenter.js
+++ src/Components/AdminPortal/NewRenter/NewRenter.js
@@ -1,28 +1,142 @@
-import React from 'react'
-import './NewRenter.scss'
-function NewRenter() {
-    return (
-        <div>
-            <form className="new-renter-form">
-              <label for="firstName">First Name</label>
-              <input name="firstName" type="text" />
-              <label for="lastName">Last Name</label>
-              <input name="firstName" type="text" />
-              <label for="email">Email</label>
-              <input name="email" type="email" />
-              <label for="phoneNumber">Phone Number</label>
-              <input name="phoneNumber" type="text" />
-              <label for="username">Username</label>
-              <input name="username" type="text" />
-              <label for="password">Password</label>
-              <input name="password" type="password" />
-              <label for="occupants">Occupants</label>
-              <input name="occupants" type="number" />
-              <label for='adminCheckbox'>Admin:</label>
-              <input name='adminCheckbox' type="checkbox" />
-            </form>
-        </div>
-    )
-}
+import React, { useState, useEffect } from "react";
+import swal from "sweetalert";
 
-export default NewRenter
+import "./NewRenter.scss";
+const NewRenter = () => {
+  //Declare state variables
+  const [adminChecked, toggleAdminChecked] = useState(false);
+  const [generatedUsername, setGeneratedUsername] = useState("");
+  const [newRenterForm, setNewRenterForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    username: "",
+    password: "",
+    occupants: "",
+    isadmin: false,
+  });
+  //watches for last digit of phone number to generate a username
+  useEffect(() => {
+    generateUsername();
+  }, [newRenterForm.phoneNumber]);
+
+  //generates a username off of the firstName lastName and last four digits of the phone number. Can be changed if desired.
+  const generateUsername = async () => {
+    if (newRenterForm.phoneNumber.length > 9) {
+      let lastFour = /[0-9]{4}$/;
+      let lastFourPhone = lastFour.exec(newRenterForm.phoneNumber);
+      await setGeneratedUsername(
+        newRenterForm.firstName + newRenterForm.lastName + lastFourPhone
+      );
+      await setNewRenterForm({ ...newRenterForm, username: generatedUsername });
+    }
+  };
+  //handles all input fields and setting their values to state.
+  const inputHandler = (e) => {
+    setNewRenterForm({ ...newRenterForm, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <form className="new-renter-form">
+        <section>
+          <label htmlFor="firstName">
+            First Name
+            <input name="firstName" type="text" onChange={inputHandler} />
+          </label>
+
+          <label htmlFor="lastName">
+            Last Name
+            <input name="lastName" type="text" onChange={inputHandler} />
+          </label>
+
+          <label htmlFor="email">
+            Email
+            <input name="email" type="email" onChange={inputHandler} />
+          </label>
+
+          <label htmlFor="phoneNumber">
+            Phone Number
+            <input
+              name="phoneNumber"
+              type="tel"
+              maxLength="10"
+              onChange={inputHandler}
+            />
+          </label>
+
+          <label htmlFor="username">
+            Username
+            <input
+              value={newRenterForm.username}
+              name="username"
+              type="text"
+              onFocus={generateUsername}
+              onChange={(e) => {
+                setGeneratedUsername(e.target.value);
+                inputHandler(e);
+              }}
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input name="password" type="password" onChange={inputHandler} />
+          </label>
+          <label htmlFor="occupants">
+            Occupants
+            <input name="occupants" type="number" onChange={inputHandler} />
+          </label>
+          <label className="admin-checkbox-container" htmlFor="adminCheckbox">
+            Admin:{" "}
+            {adminChecked ? (
+              <div>
+                <button
+                  type="button"
+                  name="adminCheckbox"
+                  id="admin-button-checked"
+                  onClick={() => toggleAdminChecked(!adminChecked)}
+                >
+                  Yes
+                </button>
+                <span>
+                  Click if you wish to remove admin controls from this user.
+                </span>
+              </div>
+            ) : (
+              <div>
+                <button
+                  name="adminCheckbox"
+                  id="admin-button-not-checked"
+                  type="button"
+                  onClick={() => {
+                    swal({
+                      title: "Warning!",
+                      text:
+                        "Checking this box will alow this user admin access!",
+                      icon: "warning",
+                      buttons: true,
+                      dangerMode: true,
+                    }).then((okay) => {
+                      if (okay) {
+                        toggleAdminChecked(!adminChecked);
+                      } else {
+                        swal("This user was not marked as an admin.");
+                      }
+                    });
+                  }}
+                >
+                  No
+                </button>
+                <div>Click if you wish to give this user admin controls.</div>
+              </div>
+            )}
+          </label>
+          <button id="submit-new-renter-btn">Submit New Renter Form</button>
+        </section>
+      </form>
+    </div>
+  );
+};
+
+export default NewRenter;
