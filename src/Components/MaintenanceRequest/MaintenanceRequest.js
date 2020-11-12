22faba4285b1e810656366980bf00b1b9925de78
Dallin <dallinpoulson@gmail.com>
2020-11-12 11:38:43 -0700

    removed warnings and console logs on MR

--- src/Components/MaintenanceRequest/MaintenanceRequest.js
+++ src/Components/MaintenanceRequest/MaintenanceRequest.js
@@ -1,16 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
-import './MaintenanceRequest.scss'
+import "./MaintenanceRequest.scss";
 import swal from "sweetalert";
 
-import {
-  Form,
-  Button,
-  ToggleButtonGroup,
-  ToggleButton,
-  Row,
-  Col,
-} from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import Axios from "axios";
 
 const MaintenanceRequest = (props) => {
@@ -28,98 +21,125 @@ const MaintenanceRequest = (props) => {
   ];
   const [emergency, toggleEmergency] = useState(false);
   const [problemAreaArray, setproblemAreaArray] = useState([]);
-  const [selectedproblemArea, setSelectedproblemArea] = useState({})
-  const [requestDetails, setRequestDetails] = useState('')
-  const [phoneNumber, setPhoneNumber] = useState('')
-  const [email, setEmail] = useState('')
-  const [userInfo, setUserInfo] = useState({})
+  const [selectedproblemArea, setSelectedproblemArea] = useState({});
+  const [requestDetails, setRequestDetails] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [userInfo, setUserInfo] = useState({});
   useEffect(() => {
-    getUserInfo()
-    }
-  , []);
+    getUserInfo();
+  }, []);
 
   const getUserInfo = () => {
-    Axios.get('/api/user').then((res) => {
-      console.log(res.data)
-      setUserInfo(res.data)
-    })
-  }
+    Axios.get("/api/user").then((res) => {
+      setUserInfo(res.data);
+    });
+  };
   const problemAreaToggler = (problemArea) => {
-            if(!problemAreaArray.includes(problemArea)){
-              problemAreaArray.push(problemArea)
-            } else {
-              let problemAreaIndex = problemAreaArray.findIndex((element) => element === problemArea)
-              problemAreaArray.splice(problemAreaIndex,1)
-            }
-            console.log( problemAreaArray)
-    
+    if (!problemAreaArray.includes(problemArea)) {
+      problemAreaArray.push(problemArea);
+    } else {
+      let problemAreaIndex = problemAreaArray.findIndex(
+        (element) => element === problemArea
+      );
+      problemAreaArray.splice(problemAreaIndex, 1);
+    }
   };
 
-  console.log('rD',requestDetails)
-  console.log('pN',phoneNumber)
-  console.log('email',email)
+
   const submitRequest = (e) => {
     e.preventDefault();
     let property_id = userInfo.property_id;
-    Axios.post('/api/request', {property_id, problemAreaArray, emergency, requestDetails, phoneNumber, email}).then((res) => {
-      swal("Success", "Your request was submitted!", "success")
-      props.history.push('/portal')
-    })
-  }
+    Axios.post("/api/request", {
+      property_id,
+      problemAreaArray,
+      emergency,
+      requestDetails,
+      phoneNumber,
+      email,
+    }).then((res) => {
+      swal("Success", "Your request was submitted!", "success");
+      props.history.push("/portal");
+    });
+  };
 
-  
   return (
     <div>
-      <Form onSubmit={(e) => submitRequest(e)} id='request-form'>
+      <Form onSubmit={(e) => submitRequest(e)} id="request-form">
         <section>
-          <Form.Text className='request-text-prompts'>For: {userInfo.street_address ? `${userInfo.street_address}, ${userInfo.city}, ${userInfo.state}` : null}</Form.Text>
+          <Form.Text className="request-text-prompts">
+            For:{" "}
+            {userInfo.street_address
+              ? `${userInfo.street_address}, ${userInfo.city}, ${userInfo.state}`
+              : null}
+          </Form.Text>
         </section>
-        <section
-          className='emergency-button'
+        <section className="emergency-button">
+          <Form.Text className="request-text-prompts">
+            Select all that apply:
+          </Form.Text>
+
+          <Button
+            variant="light"
+            id={emergency ? "emergency-button" : "unselected-emergency-button"}
+            onClick={() => toggleEmergency(!emergency)}
           >
-          <Form.Text  
-          className='request-text-prompts'
-          >Select all that apply:</Form.Text>
-
-          <Button 
-          variant='light'  
-          id={emergency ? 'emergency-button' : 'unselected-emergency-button'}
-          onClick={() => toggleEmergency(!emergency)}>EMERGENCY</Button>
+            EMERGENCY
+          </Button>
         </section>
-        <section id='problem-areas-list'>
-        {problemAreas.map((problemArea) => {
-          let capitalized = problemArea.charAt(0).toUpperCase() + problemArea.slice(1);
-          return <Button 
-              id={problemAreaArray.includes(problemArea) ? 'selected-button' : ''
-            }
-              variant='light'
-              onClick={() => {
-              setSelectedproblemArea(problemArea)
-              console.log(selectedproblemArea)
-              problemAreaToggler(problemArea)}}>{capitalized}</Button>;
-        })}
-
+        <section id="problem-areas-list">
+          {problemAreas.map((problemArea) => {
+            let capitalized =
+              problemArea.charAt(0).toUpperCase() + problemArea.slice(1);
+            return (
+              <Button
+                id={
+                  problemAreaArray.includes(problemArea)
+                    ? "selected-button"
+                    : ""
+                }
+                variant="light"
+                onClick={() => {
+                  setSelectedproblemArea(problemArea);
+                  problemAreaToggler(problemArea);
+                }}
+              >
+                {capitalized}
+              </Button>
+            );
+          })}
         </section>
-        <Form.Group >
-          <Form.Text className='request-text-prompts' >Please explain your request in detail:</Form.Text>
-          <Form.Control name='requestDetails' as='textarea' rows='3' placeholder="Explain here." onChange={(e) => setRequestDetails(e.target.value)}/>
+        <Form.Group>
+          <Form.Text className="request-text-prompts">
+            Please explain your request in detail:
+          </Form.Text>
+          <Form.Control
+            name="requestDetails"
+            as="textarea"
+            rows="3"
+            placeholder="Explain here."
+            onChange={(e) => setRequestDetails(e.target.value)}
+          />
         </Form.Group>
-        <Form.Group >
-          <Form.Text
-          className='request-text-prompts'
-          >
+        <Form.Group>
+          <Form.Text className="request-text-prompts">
             Please upload a picture of the problem if necessary.
           </Form.Text>
           <Form.File />
-          <Form.Text
-          className='request-text-prompts'
-          >
-            Please leave the best contact information so we can get back to you regarding this request.
+          <Form.Text className="request-text-prompts">
+            Please leave the best contact information so we can get back to you
+            regarding this request.
           </Form.Text>
-          <Form.Control onChange={(e) => setPhoneNumber(e.target.value)} placeholder="Phone Number" />
-          <Form.Control onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+          <Form.Control
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder="Phone Number"
+          />
+          <Form.Control
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
         </Form.Group>
-        <Button variant="light" type="submit" >
+        <Button variant="light" type="submit">
           Submit
         </Button>
       </Form>
